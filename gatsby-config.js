e53dbd107480ffd8932e122546f07da19800a4c1
Giovanni Benussi <giovanni.benussi@usach.cl>
2020-11-03 08:45:51 -0300

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Giovanni Benussi Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Giovanni Benussi`,
+      summary: `a software developer focused on craft user interfaces`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    siteUrl: `https://www.giovannibenussi.com`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `giovannibenussi`,
     },
   },
   plugins: [
