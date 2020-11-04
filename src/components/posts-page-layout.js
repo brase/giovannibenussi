52621e4ace8dc75622c64e8fee6e1cd6e44b3db7
Giovanni Benussi <giovanni.benussi@usach.cl>
2020-11-04 19:31:27 -0300

    Add layout and seo component to post layout

--- src/components/posts-page-layout.js
+++ src/components/posts-page-layout.js
@@ -3,22 +3,38 @@ import { graphql } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const shortcodes = { Link } // Provide common components here
 
-export default function PageTemplate({ data: { mdx } }) {
+export default function PageTemplate(props) {
+  const { data  } = props
+  const { mdx } = data
+  const siteTitle = data?.site.siteMetadata.title || `Title`
+
   return (
-    <div>
+    <Layout location={location} title={siteTitle}>
+      <SEO
+        title={mdx.frontmatter.title}
+        description={mdx.frontmatter.description || mdx.excerpt}
+      />
       <h1>{mdx.frontmatter.title}</h1>
       <MDXProvider components={shortcodes}>
         <MDXRenderer>{mdx.body}</MDXRenderer>
       </MDXProvider>
-    </div>
+    </Layout>
   )
 }
 
 export const pageQuery = graphql`
   query BlogPostQuery($id: String) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+
     mdx(id: { eq: $id }) {
       id
       body
