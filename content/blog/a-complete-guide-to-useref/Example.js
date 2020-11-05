19151ccb09f1dc5253126fbbb91143ae9193886e
Giovanni Benussi <giovanni.benussi@usach.cl>
2020-11-04 23:57:12 -0300

    Add interactive example to a complete guide to useRef

---
+++ content/blog/a-complete-guide-to-useref/Example.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react"
+import styles from "./styles.module.css"
+
+let clicks = 0
+
+export default function App() {
+  const [dummy, setDummy] = useState(0)
+
+  return (
+    <div className={styles.wrapper}>
+      <button className={styles.button} onClick={() => (clicks += 1)}>
+        Clicks: {clicks}
+      </button>
+      <button className={styles.button} onClick={() => setDummy(dummy + 1)}>
+        Re-render
+      </button>
+    </div>
+  )
+}
