4bb89641136cd91876434309f466f1321979a089
Giovanni Benussi <giovanni.benussi@usach.cl>
2020-12-01 09:38:10 -0300

    Try to fix metatags not detected by Facebook

---
+++ gatsby-ssr.js
@@ -0,0 +1,22 @@
+// https://github.com/gatsbyjs/gatsby/issues/9979
+exports.onPreRenderHTML = function onPreRenderHTML({
+  getHeadComponents,
+  replaceHeadComponents,
+}) {
+  const headComponents = getHeadComponents()
+  headComponents.sort((a, b) => {
+    if (a.type === b.type || (a.type !== "style" && b.type !== "style")) {
+      return 0
+    }
+
+    if (a.type === "style") {
+      return 1
+    } else if (b.type === "style") {
+      return -1
+    }
+
+    return 0
+  })
+
+  replaceHeadComponents(headComponents)
+}
