b6657b520dbf7e4e138b397b0324cfd48d6219b8
Giovanni Benussi <giovanni.benussi@usach.cl>
2020-12-15 22:46:00 -0300

    Update first example of useRef article

--- content/blog/a-complete-guide-to-useref/Examples/OutsideVariable/multiple.js
+++ content/blog/a-complete-guide-to-useref/Examples/OutsideVariable/multiple.js
@@ -6,7 +6,13 @@ import RefButton from "../Buttons/RefButton"
 const xTimes = (x, callback) => [...Array(x)].map(callback)
 import c from "classnames"
 
-export default function App({ refs = 0, state = 0, variable = 0 }) {
+export default function App({
+  displayType = true,
+  displayTitles = true,
+  refs = 0,
+  state = 0,
+  variable = 0,
+}) {
   const [dummy, setDummy] = useState(0)
 
   return (
@@ -16,28 +22,32 @@ export default function App({ refs = 0, state = 0, variable = 0 }) {
         "bg-gray-100 p-4 m-auto w-max rounded mt-4 mb-4"
       )}
     >
-      <strong>Type</strong>
-      <strong>Result</strong>
+      {displayTitles && (
+        <>
+          <strong>Type</strong>
+          <strong>Result</strong>
+        </>
+      )}
       {xTimes(variable, i => (
         <React.Fragment key={i}>
-          <span>outside variable</span>
+          {displayType && <span>outside variable</span>}
           <OutsideButton />
         </React.Fragment>
       ))}
       {xTimes(state, i => (
         <React.Fragment key={i}>
-          <span>state</span>
+          {displayType && <span>state</span>}}
           <StateButton />
         </React.Fragment>
       ))}
       {xTimes(refs, i => (
         <React.Fragment key={i}>
-          <span>ref</span>
+          {displayType && <span>ref</span>}
           <RefButton />
         </React.Fragment>
       ))}
       <button
-        className="btn btn-purple col-span-2"
+        className={c("btn btn-purple", displayType && "col-span-2")}
         onClick={() => setDummy(dummy + 1)}
       >
         Re-render
